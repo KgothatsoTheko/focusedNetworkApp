18d56c9cd41e635ffb7a5b297ea8300b441fe340
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-11-22 00:08:16 +0200

    ios and android updates pt1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,15 +18,6 @@ import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
   declarations: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    // AngularFireModule.initializeApp({
-    //   apiKey: 'AIzaSyBk8rs0eMKpVRrWSyWhHekE_ZYt2-cDPiw',
-    // authDomain: 'focus-network-eca6d.firebaseapp.com',
-    // projectId: 'focus-network-eca6d',
-    // storageBucket: 'focus-network-eca6d.appspot.com',
-    // messagingSenderId: '525879076713',
-    // appId: '1:525879076713:android:809b678fe47ad1ef643ff8',
-    // }),
-    // AngularFireMessagingModule, 
     ReactiveFormsModule, FormsModule, HttpClientModule,
     IonicStorageModule.forRoot()
   ],
--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,6 @@ import { getMessaging } from 'firebase/messaging';
 
 import { Capacitor } from '@capacitor/core';
 
-// // Firebase configuration
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBk8rs0eMKpVRrWSyWhHekE_ZYt2-cDPiw',
-//   authDomain: 'focus-network-eca6d.firebaseapp.com',
-//   projectId: 'focus-network-eca6d',
-//   storageBucket: 'focus-network-eca6d.appspot.com',
-//   messagingSenderId: '525879076713',
-//   appId: '1:525879076713:android:809b678fe47ad1ef643ff8',
-// };
-
-
-// // Ensure platform-specific logic
-// if (Capacitor.getPlatform() === 'android' || Capacitor.getPlatform() === 'ios') {
-//   const app = initializeApp(firebaseConfig);
-//   const messaging = getMessaging(app);
-// }
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
